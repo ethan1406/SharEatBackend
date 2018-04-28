ded314519c3d1024a528ee7e2645575b9df62505
Ethan Chang <ethanc@usc.edu>
2018-04-27 19:01:54 -0700

    facebook url

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '775610585933474', // your App ID
         'clientSecret'  : '8ce931d8eb2cf5c67ea80968b171740d', // your App Secret
-        'callbackURL'   : 'shareatpayment.us-west-1.elasticbeanstalk.com/auth/facebook/callback'
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
         //'https://refriendonline.herokuapp.com/auth/facebook/callback'
     }
 };
