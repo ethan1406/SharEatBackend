ba2e6a07fbd7b84923744ed68202afca7498e5f8
Ethan Chang <ethanc@usc.edu>
2018-05-19 23:05:07 -0700

    updating server to support local authentication

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import config from './config';
 // import path from 'path';
+import morgan from 'morgan';
 import passport from 'passport';
 import express from 'express';
 import flash from 'connect-flash';
 import session from 'express-session';
 import mongoose from 'mongoose';
-import User from './models/user';
 import bodyParser from 'body-parser';
 
 
@@ -21,10 +21,11 @@ db.on('error', console.error.bind(console, '# MongoDB - connection error: '));
 export default db;
 
 
-
-
+//setting up express
+server.use(morgan('dev'));
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: false }));
+server.use(flash());
 
 
 require('./config/passport')(passport);
@@ -38,8 +39,10 @@ server.use(session({
 server.use(passport.initialize());
 server.use(passport.session());
 
-server.use(flash());
 
+
+//testing purposes
+server.set('view engine', 'ejs');
 
 server.get('/auth/facebook', passport.authenticate('facebook', { scope : ['email', 'user_friends'],
 																failureFlash: true ,
@@ -50,23 +53,51 @@ server.get('/auth/facebook/callback',
         passport.authenticate('facebook', {
             successRedirect : '/loginsuccess',
             failureRedirect : '/login',
-
-
 }));
+
+
+
+server.get('/signup', function(req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.render('signup.ejs', { message: req.flash('signupMessage') });
+    });
+
+server.get('/login', (req,res)=> {
+	res.render('login.ejs', { message: req.flash('loginMessage') });
+});
+
+
+server.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/loginsuccess', // redirect to the secure profile section
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+ }));
+
+
+server.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/loginsuccess', // redirect to the secure profile section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+}));
+
+
+
+
 
 server.get('/test', (req, res) =>
 {
 	res.send('sup');
 });
 
-server.get('/testdb', (req, res) =>
-{
-	var newUser = new User();
-	newUser.id = '12345';
-	newUser.name = 'Ethan Chang';
-	newUser.save();
-	res.send('here');
-});
+// server.get('/testdb', (req, res) =>
+// {
+// 	var newUser = new User();
+// 	newUser.id = '12345';
+// 	newUser.name = 'Ethan Chang';
+// 	newUser.save();
+// 	res.send('here');
+// });
 
 
 // function loggedIn(req, res, next) {
@@ -78,9 +109,7 @@ server.get('/testdb', (req, res) =>
 //     }
 // }
 
-server.get('/login', (req,res)=> {
-	res.send('youre not logged in');
-});
+
 
 server.get('/loginsuccess', (req,res)=> {
 	res.send('youre logged in');
